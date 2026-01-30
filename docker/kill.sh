f091824184ec9c799afc9f5c8a959f34fd8f8afa
Murilo Preto <murilopreto2003@outlook.com>
2026-01-30 15:55:34 -0300

    initial commit

---
+++ docker/kill.sh
@@ -0,0 +1,1 @@
+docker kill flask_app
---
+++ flask_server/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
