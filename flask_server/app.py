c030777ceccc2a339e89000e7e44f20a373dd8f5
Murilo Preto <murilopreto2003@outlook.com>
2026-02-01 16:14:09 -0300

    add navigation indication

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
-
 
 @app.route("/")
 def home():
@@ -16,6 +15,10 @@ def cv():
 @app.route("/naninha")
 def naninha():
     return render_template("naninha.html")
+
+@app.context_processor
+def inject_current_path():
+    return dict(current_path=request.path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
